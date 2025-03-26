fafdad2666250bf89699e49d2c915a34da09d6a9
Mohamed Haki <Mohamedhaki70@gmail.com>
2025-03-26 10:07:48 +0000

    LOC-9: refactor: update automobile module

--- src/automobile/automobile.module.ts
+++ src/automobile/automobile.module.ts
@@ -2,28 +2,28 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CategoryController } from './controllers/category.controller';
 import { AutomobileController } from './controllers/automobile.controller';
-import { MaintenanceController } from './controllers/maintenance.controller';
 import { CategoryService } from './services/category.service';
 import { AutomobileService } from './services/automobile.service';
-import { MaintenanceService } from './services/maintenance.service';
 import { Category, CategorySchema } from './schemas/category.schema';
 import { Automobile, AutomobileSchema } from './schemas/automobile.schema';
-import { Maintenance, MaintenanceSchema } from './schemas/maintenance.schema';
 import { User, UserSchema } from '../auth/schemas/user.schema';
 import { AuthModule } from '../auth/auth.module';
 import { MailModule } from '../mail/mail.module';
 import { SmsModule } from '../sms/sms.module';
 import { CommonModule } from '../common/common.module';
 import { FileService } from '../common/services/file.service';
+import { ScheduleModule } from '@nestjs/schedule';
+import { Reservation, ReservationSchema } from '../reservation/schemas/reservation.schema';
 
 @Module({
     imports: [
         MongooseModule.forFeature([
             { name: Category.name, schema: CategorySchema },
             { name: Automobile.name, schema: AutomobileSchema },
-            { name: Maintenance.name, schema: MaintenanceSchema },
-            { name: User.name, schema: UserSchema }
+            { name: User.name, schema: UserSchema },
+            { name: Reservation.name, schema: ReservationSchema }
         ]),
+        ScheduleModule.forRoot(), // Pour les tâches planifiées
         AuthModule,
         MailModule,
         SmsModule,
@@ -32,18 +32,15 @@ import { FileService } from '../common/services/file.service';
     controllers: [
         CategoryController,
         AutomobileController,
-        MaintenanceController
     ],
     providers: [
         CategoryService,
         AutomobileService,
-        MaintenanceService,
         FileService
     ],
     exports: [
         CategoryService,
         AutomobileService,
-        MaintenanceService
     ]
 })
 export class AutomobileModule {} 
